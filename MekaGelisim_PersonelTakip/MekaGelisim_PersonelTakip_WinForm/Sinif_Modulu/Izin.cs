e7d5b4a2010b3c5f4e47160dc21a2cb4201ada10
bnymnclskn01 <bnymncliskn1903@gmail.com>
2021-05-13 22:57:26 +0300

    Yeni Dersler Geldi Çıtır Çıtır

---
+++ MekaGelisim_PersonelTakip/MekaGelisim_PersonelTakip_WinForm/Sinif_Modulu/Izin.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MekaGelisim_PersonelTakip_WinForm.Sinif_Modulu
+{
+    public class Izin
+    {
+        #region Private Properties
+        private int _izinID;
+        private int _personelID;
+        private string _izinTipi;
+        private double _izinGunSayisi;
+        private string _izinBaslangic;
+        private string _izinBitis;
+        #endregion
+        #region Public Properties
+        public int IzinID
+        {
+            get { return _izinID; }
+            set { _izinID = value; }
+        }
+        public int PersonelID
+        {
+            get { return _personelID; }
+            set { _personelID = value; }
+        }
+        public string IzinTipi
+        {
+            get { return _izinTipi; }
+            set { _izinTipi = value; }
+        }
+        public double IzinGunSayisi
+        {
+            get { return _izinGunSayisi; }
+            set { _izinGunSayisi = value; }
+        }
+        public string IzinBaslangic
+        {
+            get { return _izinBaslangic; }
+            set { _izinBaslangic = value; }
+        }
+        public string IzinBitis
+        {
+            get { return _izinBitis; }
+            set { _izinBitis = value; }
+        }
+        #endregion
+        #region Connection
+        SqlConnection conn = new SqlConnection(Genel.connStr);
+        #endregion
+        #region SQL SORGULAR
+        public void IzinlerGetir(int ID, ListView liste)
+        {
+            liste.Items.Clear();
+            SqlCommand cmd = new SqlCommand("Select ID,PersonelAdi,PersonelSoyadi,İzinTipi,İzinGunSayisi,İzinBaslangic,İzinBitis from Tbl_Personel p inner join Tbl_IzinHareketleri ih on p.ID=ih.PersonelID where ih.PersonelID=@ID and ih.Silindi=0", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            if (conn.State==ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                int i = 0;
+                while (dr.Read())
+                {
+                    liste.Items.Add(dr[0].ToString());
+                    liste.Items[i].SubItems.Add(dr[1].ToString());
+                    liste.Items[i].SubItems.Add(dr[2].ToString());
+                    liste.Items[i].SubItems.Add(dr[3].ToString());
+                    liste.Items[i].SubItems.Add(dr[4].ToString());
+                    liste.Items[i].SubItems.Add(dr[5].ToString());
+                    liste.Items[i].SubItems.Add(dr[6].ToString());
+                    i++;
+                }
+            }
+            dr.Close();
+            conn.Close();
+        }
+        public void IzinlerGetir(ListView liste)
+        {
+            liste.Items.Clear();
+            SqlCommand cmd = new SqlCommand("Select ID,PersonelAdi,PersonelSoyadi,İzinTipi,İzinGunSayisi,İzinBaslangic,İzinBitis from Tbl_IzinHareketleri ih inner join Tbl_Personel p on ih.PersonelID=@p.ID ih.Silindi=0", conn);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                int i = 0;
+                while (dr.Read())
+                {
+                    liste.Items.Add(dr[0].ToString());
+                    liste.Items[i].SubItems.Add(dr[1].ToString());
+                    liste.Items[i].SubItems.Add(dr[2].ToString());
+                    liste.Items[i].SubItems.Add(dr[3].ToString());
+                    liste.Items[i].SubItems.Add(dr[4].ToString());
+                    liste.Items[i].SubItems.Add(dr[5].ToString());
+                    liste.Items[i].SubItems.Add(dr[6].ToString());
+                    i++;
+                }
+            }
+            dr.Close();
+            conn.Close();
+        }
+        public int IzinIDOlustur()
+        {
+            int ID = 0;
+            SqlCommand cmd = new SqlCommand("Select max(ID) from Tbl_IzinHareketleri", conn);
+            if (cmd.ExecuteScalar().ToString() == "")
+                ID = 1;
+            else
+            {
+                ID = Convert.ToInt32(cmd.ExecuteScalar()) + 1;
+            }
+            return ID;
+        }
+        public bool IzinKontrol(int ID, string Baslangic, string Bitis)
+        {
+            bool Varmi = false;
+            SqlCommand cmd = new SqlCommand("Select * From Tbl_IzinHareketleri where PersonelID=@ID and IzinBaslangic=@Baslangic and IzinBitis=@Bitis", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            cmd.Parameters.Add("@Baslangic", SqlDbType.VarChar).Value = Baslangic;
+            cmd.Parameters.Add("@Bitis", SqlDbType.VarChar).Value = Bitis;
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+                Varmi = true;
+            dr.Close();
+            conn.Close();
+            return Varmi;
+        }
+        public bool IzinKontrol(int ID, string IzinTipi , string Baslangic, string Bitis)
+        {
+            bool Varmi = false;
+            SqlCommand cmd = new SqlCommand("Select * From Tbl_IzinHareketleri where PersonelID=@ID and IzinTipi=@Tipi and IzinBaslangic=@Baslangic and IzinBitis=@Bitis", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            cmd.Parameters.Add("@Tipi", SqlDbType.VarChar).Value = IzinTipi;
+            cmd.Parameters.Add("@Baslangic", SqlDbType.VarChar).Value = Baslangic;
+            cmd.Parameters.Add("@Bitis", SqlDbType.VarChar).Value = Bitis;
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+                Varmi = true;
+            dr.Close();
+            conn.Close();
+            return Varmi;
+        }
+        public bool IzinSil(int ID)
+        {
+            bool Sonuc = false;
+            SqlCommand cmd = new SqlCommand("Update Tbl_IzinHareketleri set Silindi=1 where ID=@ID", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            try
+            {
+                Sonuc = Convert.ToBoolean(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally { conn.Close(); }
+            return Sonuc;
+        }
+        #endregion
+    }
+}
--- MekaGelisim_PersonelTakip/MekaGelisim_PersonelTakip_WinForm/Sinif_Modulu/Personel.cs
+++ MekaGelisim_PersonelTakip/MekaGelisim_PersonelTakip_WinForm/Sinif_Modulu/Personel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +10,17 @@ namespace MekaGelisim_PersonelTakip_WinForm.Sinif_Modulu
 {
     public class Personel
     {
+        #region Private Properties
         private int _personelID;
         private string _tckn;
         private string _personelAd;
         private string _personelSoyad;
         private string _telefon;
         private string _departman;
+        private string _adres
         private string _durum;
         private string _iban;
-        private double _SabitMaas;
+        private double _sabitMaas;
         private string _girisTarih;
         private string _cikisTarih;
         private int _toplamRaporlu;
@@ -24,8 +28,7 @@ namespace MekaGelisim_PersonelTakip_WinForm.Sinif_Modulu
         private int _ToplamUcretsiz;
         private double _toplamAlacak;
         private double _toplamBorc;
-
-
+        #endregion
         #region Properties
         public int PersonelID
         {
@@ -36,7 +39,236 @@ namespace MekaGelisim_PersonelTakip_WinForm.Sinif_Modulu
         {
             get { return _tckn; }
             set { _tckn = value; }
-        } 
+        }
+        public string Adres
+        {
+            get { return _adres; }
+            set { _adres = value; }
+        }
+        public string PersonelAd
+        {
+            get { return _personelAd; }
+            set { _personelAd = value; }
+        }
+        public string PersonelSoyad
+        {
+            get { return _personelSoyad; }
+            set { _personelSoyad = value; }
+        }
+        public string Telefon
+        {
+            get { return _telefon; }
+            set { _telefon = value; }
+        }
+        public string Departman
+        {
+            get { return _departman; }
+            set { _departman = value; }
+        }
+        public string Durum
+        {
+            get { return _durum; }
+            set { _durum = value; }
+        }
+        public string Iban
+        {
+            get { return _iban; }
+            set { _iban = value; }
+        }
+        public double SabitMaas
+        {
+            get { return _sabitMaas ; }
+            set { _sabitMaas = value; }
+        }
+        public string GirisTarihi
+        {
+            get { return _girisTarih; }
+            set { _girisTarih = value; }
+        }
+        public string CikisTarihi
+        {
+            get { return _cikisTarih; }
+            set { _cikisTarih = value; }
+        }
+        public int ToplamRaporlu
+        {
+            get { return _toplamRaporlu; }
+            set { _toplamRaporlu = value; }
+        }
+        public int ToplamUcretli
+        {
+            get { return _toplamUcretli; }
+            set { _toplamUcretli = value; }
+        }
+        public int ToplamUcretsiz
+        {
+            get { return _ToplamUcretsiz; }
+            set { _ToplamUcretsiz = value; }
+        }
+        public double ToplamAlacak
+        {
+            get { return _toplamAlacak; }
+            set { _toplamAlacak = value; }
+        }
+        public double ToplamBorc
+        {
+            get { return _toplamBorc; }
+            set { _toplamBorc = value; }
+        }
         #endregion
+        #region Personel Sql Sorgular
+        SqlConnection conn = new SqlConnection(Genel.connStr);
+        public bool PersonelEkle(Personel p)
+        {
+            bool sonuc = false;
+            SqlCommand cmd = new SqlCommand("insert into Tbl_Personel(TCKN,PersonelAdi,PersonelSoyadi,Telefon,Adres,Departman,Durum,IBAN,SabitMaas,GirisTarihi,CikisTarihi,GunlukMesai,ToplamRaporlu,ToplamUcretli,ToplamUcretsiz,ToplamAlacak,ToplamBorc) values(@TCKN,@PersonelAdi,@PersonelSoyadi,@Telefon,@Adres,@Departman,@Durum,@IBAN,@SabitMaas,@GirisTarihi,@CikisTarihi,@GunlukMesai,@ToplamRaporlu,@ToplamUcretli,@ToplamUcretsiz,@ToplamAlacak,@ToplamBorc)", conn);
+            cmd.Parameters.Add("@TCKN", SqlDbType.VarChar).Value = p._tckn;
+            cmd.Parameters.Add("@PersonelAdi", SqlDbType.VarChar).Value = p._personelAd;
+            cmd.Parameters.Add("@PersonelSoyadi", SqlDbType.VarChar).Value = p._personelSoyad;
+            cmd.Parameters.Add("@Telefon", SqlDbType.VarChar).Value = p._telefon;
+            cmd.Parameters.Add("@Adres", SqlDbType.VarChar).Value = p._adres;
+            cmd.Parameters.Add("@Departman", SqlDbType.VarChar).Value = p._departman;
+            cmd.Parameters.Add("@Durum", SqlDbType.VarChar).Value = p._durum;
+            cmd.Parameters.Add("@IBAN", SqlDbType.VarChar).Value = p._iban;
+            cmd.Parameters.Add("@SabitMaas", SqlDbType.Money).Value = p._sabitMaas;
+            cmd.Parameters.Add("@GirisTarihi", SqlDbType.DateTime).Value = p._girisTarih;
+            cmd.Parameters.Add("@GirisTarihi", SqlDbType.DateTime).Value = Convert.ToDateTime("31:12.2199 00:00:00.000");
+            if (conn.State==ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                sonuc = Convert.ToBoolean(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally{conn.Close(); }
+            return sonuc;
+        }
+        public bool PersonelGuncelleWithLeft(Personel p)
+        {
+            bool Sonuc = false;
+            SqlCommand cmd = new SqlCommand("Update Tbl_Personel set TCKN=@TCKN, PersonelAdi=@PersonelAdi, PersonelSoyadi=@PersonelSoyadi,Telefon=@Telefon,Adres=@Adres,Departman=@Departman,Durum=@Durum,IBAN=@IBAN,SabitMaas=@SabitMaas,GirisTarihi=@GirisTarihi,CikisTarihi=@CikisTarihi, where ID=@ID", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = p._personelID;
+            cmd.Parameters.Add("@TCKN", SqlDbType.VarChar).Value = p._tckn;
+            cmd.Parameters.Add("@PersonelAdi", SqlDbType.VarChar).Value = p._personelAd;
+            cmd.Parameters.Add("@PersonelSoyadi", SqlDbType.VarChar).Value = p._personelSoyad;
+            cmd.Parameters.Add("@Telefon", SqlDbType.VarChar).Value = p._telefon;
+            cmd.Parameters.Add("@Adres", SqlDbType.VarChar).Value = p._adres;
+            cmd.Parameters.Add("@Departman", SqlDbType.VarChar).Value = p._departman;
+            cmd.Parameters.Add("@Durum", SqlDbType.VarChar).Value = p._durum;
+            cmd.Parameters.Add("@IBAN", SqlDbType.VarChar).Value = p._iban;
+            cmd.Parameters.Add("@SabitMaas", SqlDbType.Money).Value = p._sabitMaas;
+            cmd.Parameters.Add("@GirisTarihi", SqlDbType.DateTime).Value = Convert.ToDateTime(p._girisTarih);
+            cmd.Parameters.Add("@CikisTarihi", SqlDbType.DateTime).Value = Convert.ToDateTime(p._cikisTarih);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                Sonuc = Convert.ToBoolean(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally { conn.Close(); }
+            return Sonuc;
+        }
+        public bool PersonelGuncelleWithoutLeft(Personel p)
+        {
+            bool Sonuc = false;
+            SqlCommand cmd = new SqlCommand("Update Tbl_Personel set TCKN=@TCKN, PersonelAdi=@PersonelAdi, PersonelSoyadi=@PersonelSoyadi,Telefon=@Telefon,Adres=@Adres,Departman=@Departman,Durum=@Durum,IBAN=@IBAN,SabitMaas=@SabitMaas,GirisTarihi=@GirisTarihi,CikisTarihi=@CikisTarihi, where ID=@ID", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = p._personelID;
+            cmd.Parameters.Add("@TCKN", SqlDbType.VarChar).Value = p._tckn;
+            cmd.Parameters.Add("@PersonelAdi", SqlDbType.VarChar).Value = p._personelAd;
+            cmd.Parameters.Add("@PersonelSoyadi", SqlDbType.VarChar).Value = p._personelSoyad;
+            cmd.Parameters.Add("@Telefon", SqlDbType.VarChar).Value = p._telefon;
+            cmd.Parameters.Add("@Adres", SqlDbType.VarChar).Value = p._adres;
+            cmd.Parameters.Add("@Departman", SqlDbType.VarChar).Value = p._departman;
+            cmd.Parameters.Add("@Durum", SqlDbType.VarChar).Value = p._durum;
+            cmd.Parameters.Add("@IBAN", SqlDbType.VarChar).Value = p._iban;
+            cmd.Parameters.Add("@SabitMaas", SqlDbType.Money).Value = p._sabitMaas;
+            cmd.Parameters.Add("@GirisTarihi", SqlDbType.DateTime).Value = Convert.ToDateTime(p._girisTarih);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                Sonuc = Convert.ToBoolean(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally { conn.Close(); }
+            return Sonuc;
+        }
+        public bool PersonelKontrol(string TCKN)
+        {
+            bool varMi = false;
+            SqlCommand cmd = new SqlCommand("Select * From Tbl_Personel where TCKN=@TCKN", conn);
+            cmd.Parameters.Add("@TCKN", SqlDbType.VarChar).Value = TCKN;
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+                varMi = true;
+            dr.Close();
+            conn.Close();
+            return varMi;
+        }
+        public bool PersonelSil(int ID)
+        {
+            bool Sonuc = false;
+            SqlCommand cmd = new SqlCommand("Update Tbl_Personel set Silindi=1 where ID=@ID", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                Sonuc = Convert.ToBoolean(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally { conn.Close(); }
+            return Sonuc;
+        }
+        public bool PersonelTamSil(int ID)
+        {
+            bool Sonuc = false;
+            SqlCommand cmd = new SqlCommand("Delete From Tbl_Personel where ID=@ID", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                Sonuc = Convert.ToBoolean(cmd.ExecuteNonQuery());
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally { conn.Close(); }
+            return Sonuc;
+        }
+        #endregion
+
+        /*
+
+         bir veri eklemek istediğinizde
+        1 veya 0 döndürür
+        Eğer 1 dönerse veri eklenir güncellenir silinir veri getirilir  veri tabanından executeNonQuery 1 - 0 SqlException Bunu görmek için sqlException hatası patlatıyoruz.
+         */
     }
 }
